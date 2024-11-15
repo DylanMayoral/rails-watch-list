53a355c29e3ac1ec59a6452075820f87f3bf5615
Dylan Mayoral <dylanmayoral@gmail.com>
2024-11-15 12:19:23 +0900

    done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  #
+
+  root to: 'lists#index'
+
+  resources :lists do
+    resources :bookmarks
+  end
 end
